e7399574dfe1934debfa523852a01da1950966ce
Vaishnav R <vaishnavrajendran95@gmail.com>
2023-02-03 19:40:03 +0530

    reducer added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/PostsSlice'
+import usersReducer from '../features/users/UsersSlice'
 
 export const store = configureStore({
     reducer:{
-        posts:postsReducer
+        posts:postsReducer,
+        users:usersReducer,
     }
 })
--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -70,5 +70,4 @@ const AddPostForm = () => {
         </section>
     )
 }
-
 export default AddPostForm
